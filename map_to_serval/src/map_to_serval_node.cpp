eb5ca24d4aaa52035cdc51f63db05e568989d9e2
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2017-08-30 14:09:12 +0200

    Add missing check during folder creation

--- map_to_serval/src/map_to_serval_node.cpp
+++ map_to_serval/src/map_to_serval_node.cpp
@@ -156,7 +156,12 @@ public:
     // Below creates the subfolder. Is non-op after it has been created once
     boost::filesystem::path dir(p_save_folder_ + "/" + filename_ss_.str());
     ROS_DEBUG_STREAM(dir.generic_string());
-    boost::filesystem::create_directory(dir);
+
+    boost::system::error_code error_code;
+    if (!boost::filesystem::create_directory(dir, error_code)){
+      ROS_ERROR("Could not create folder %s, error_code message: %s. Aborting writing map.", dir.generic_string().c_str(), error_code.message().c_str());
+      return;
+    }
 
     // Below creates time-based subfolder.
     filename_ss_   << "/" << p_agent_name_ << "_map";;
@@ -164,7 +169,7 @@ public:
     dir = boost::filesystem::path(p_save_folder_ + "/" + filename_ss_.str());
     ROS_DEBUG_STREAM(dir.generic_string());
 
-    boost::system::error_code error_code;
+
     if (!boost::filesystem::create_directory(dir, error_code)){
       ROS_ERROR("Could not create folder %s, error_code message: %s. Aborting writing map.", dir.generic_string().c_str(), error_code.message().c_str());
       return;
